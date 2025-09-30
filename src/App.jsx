45744b8a1a5d6f0e5db2dfd33e509b219f6dd77e
Borys <ihorborys@gmail.com>
2025-09-30 16:50:40 +0300

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <div>
+                MY APP
+                <ul>
+                    <li>Пункт 1</li>
+                    <li>Пункт 2</li>
+                </ul>
+
+            </div>
+
+        </>
+    )
+}
+
+export default App
