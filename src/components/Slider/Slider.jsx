d005d4e94d2d14f0a4408d0a15d69daece8ff75c
Borys <ihorborys@gmail.com>
2025-10-02 18:24:11 +0300

    Edited responsiveness for Home page

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -42,7 +42,7 @@ export default function Slider() {
                     pagination={{clickable: true, dynamicBullets: true}}
                     navigation
                     breakpoints={{
-                        768: {slidesPerView: 2},
+                        550: {slidesPerView: 2},
                         1024: {slidesPerView: 3},
                     }}
                     className={styles.swiper}
